19697cc1619f6d8764875c76da717487e0b3a232
LCR <cariou.liam@orange.fr>
2025-06-29 16:19:00 +0200

    router du front a jour

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,16 @@ import Login from '../views/Login.vue'
 import Me from '../views/Me.vue'
 import Admin from '../views/Admin.vue'
 import Callback from '../views/Callback.vue'
+import Deposit from '../views/Deposit.vue'
+import Withdraw from '../views/Withdraw.vue'
 
 const routes = [
   { path: '/', component: Home, meta: { requiresAuth: true } },
   { path: '/login', component: Login },
   { path: '/me', component: Me, meta: { requiresAuth: true } },
   { path: '/admin', component: Admin, meta: { requiresAuth: true, admin: true } },
+  { path: '/deposit', component: Deposit, meta: { requiresAuth: true } },
+  { path: '/withdraw', component: Withdraw, meta: { requiresAuth: true } },
   { path: '/callback', component: Callback },
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
